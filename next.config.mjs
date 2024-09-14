23e0a401dcb5481abf5c76d12237caf06df04a1f
Amandeep Kaur <107187322+Amandeepkaur1804@users.noreply.github.com>
2024-09-14 17:20:35 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  // Use standalone output or serverless for Netlify compatibility
-  output: 'standalone', 
+   
 };
 
 export default nextConfig;
